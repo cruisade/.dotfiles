afa9c7de2440d525628a55abce0a36d7446f2d35
cruisade <enoughnight@gmail.com>
2020-03-14 16:59:14 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,6 +23,5 @@ ln -sn ~/.dotfiles/.config/i3 i3
 ln -sn ~/.dotfiles/.config/nvim nvim
 ln -sn ~/.dotfiles/.config/polybar polybar
 
+chsh -s $(which zsh)
 nvim +'PlugInstall --sync' +'PlugUpdate' +qa  
-
- 
